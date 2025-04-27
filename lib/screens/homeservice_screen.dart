750a19bdda7cae1b405fef8dde83ade0e9dc3722
Mulyawan13 <123245679+Mulyawan13@users.noreply.github.com>
2025-04-27 08:45:05 +0700

    tambahan

--- lib/screens/homeservice_screen.dart
+++ lib/screens/homeservice_screen.dart
@@ -682,7 +682,7 @@ class _HomeServiceScreenState extends State<HomeServiceScreen> {
     return true;
   }
 
-  void _submitForm() {
+  void _submitForm() async {
     final formData = {
       'nama': _namaController.text,
       'whatsapp': _whatsappController.text,
@@ -696,12 +696,33 @@ class _HomeServiceScreenState extends State<HomeServiceScreen> {
       'waktu_selesai': _selectedEndTime,
       'gambar': _selectedImage?.path,
     };
-    //todo : submit to API, Tunggu API selesai
-    print('Form submitted: $formData');
+    try {
+    final url = Uri.parse('https://');
 
-    ScaffoldMessenger.of(context).showSnackBar(
-      const SnackBar(content: Text('Permintaan servis berhasil dikirim')),
+    final response = await http.post(
+      url,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: json.encode(formData),
     );
 
+    if (response.statusCode == 200) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Permintaan servis berhasil dikirim')),
+      );
+      print('Response: ${response.body}');
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Gagal mengirim data: ${response.statusCode}')),
+      );
+      print('Error: ${response.body}');
+    }
+  } catch (e) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text('Terjadi error: $e')),
+    );
+    print('Exception: $e');
   }
+}
 }
